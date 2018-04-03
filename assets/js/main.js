5524a9d82f3f983ce49f5f04fef923cd675cd365
finne132 <finne132@umn.edu>
2018-04-03 10:12:53 -0500

    added id selectors for jquery

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// ID selectors
+
+// maintitle - main title div
+
+// nameEntry - div containing textbox and submit button for name entry
+    // nameTextBox - text box for name entry 
+    // nameSubmit - submit button for name entry
+
+// scoreboard - row containing 3 colums: outcome, p1score, and p2score
+    // p1score - player 1's running score display
+    // outcome - outcome of each match display 
+    // p2score - player 2's running score display
+
+// displaydata - row containing 4 columns for displaying player names and selections
+    // p1choice - player 1's choice display
+    // p2choice - player 2's choice display 
+    // p1name - player 1's name display
+    // p2name - player 2's name display 
+
+// chat - row containing 2 screen-wide columns for displaying chat and text entry box with submit button
+    // chatDisplay - div where chat is displayed 
+    // chatEntry - div containing textbox and submit button for name entry
+        // chatTextBox - text box for chat entry 
+        // chatSubmit - submit button for chat text
+
+// foot - selector for the footer
