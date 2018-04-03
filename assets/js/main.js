3b316cf66ef771a6e827807590d48046e0bd23de
finne132 <finne132@umn.edu>
2018-04-03 10:32:19 -0500

    game start state for player 1 display and database values

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,7 @@
 
 // foot - selector for the footer
 
+// firebase project configuration settings
 var config = {
     apiKey: "AIzaSyClxrsOoB1gRb2q0Cp5moMriuBUd8i9lbY",
     authDomain: "rps-multiplayer-cc7bb.firebaseapp.com",
@@ -35,6 +36,43 @@ var config = {
     storageBucket: "rps-multiplayer-cc7bb.appspot.com",
     messagingSenderId: "864869163976"
   };
-  firebase.initializeApp(config);
 
-  var database = firebase.database();
+// Initialize the FireBase Database connection
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// good resource for referencing firebase databases: 
+// https://firebase.google.com/docs/reference/js/firebase.database.Reference
+
+// listen to the players node of the database to see if anything changes
+database.ref("/players/").on("value", function(snapshot) {
+    
+    // Check to see if player 1 exists in our database - if it does, set variables and display
+    // player 1's name and data. If it doesn't exist, display waiting for player 1 
+	if (snapshot.child("player1").exists()) {
+		console.log("Player 1 exists");
+
+		// set local variables for player 1
+		p1 = snapshot.val().p1;
+		p1name = p1.name;
+
+		// display player 1's name and score data 
+		$("#p1name").text(p1name);
+		$("#p2data").html("Wins: " + p1.win + ", Losses: " + p1.loss + ", Tie: " + p1.tie);
+    } 
+
+    // what happens if player 1 doesn't exist?
+    else {
+		console.log("Player 1 does NOT exist in the database");
+		p1 = null;
+		p1name = "";
+
+		// Display "empty" state for p1 
+		$("#p1name").text("Waiting for Player 1...");
+		database.ref("/outcome/").remove();
+		$("#outcome").html("Rock! Paper! Scissors.... Shoot!");
+		$("#scoreboard").html("Waiting for Players to join");
+		$("#player1Stats").html("Win: 0, Loss: 0, Tie: 0");
+    }
+});
